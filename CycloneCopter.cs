fdabec7e6266da5357ca344860b4ab4c8df60fa3
TheMathGeek_314 <16294237+TheMathGeek314@users.noreply.github.com>
2024-10-18 05:44:15 -0500

    Add project files.

---
+++ CycloneCopter.cs
@@ -0,0 +1,33 @@
+﻿using Modding;
+using System.Collections.Generic;
+using UnityEngine;
+using HutongGames.PlayMaker;
+using HutongGames.PlayMaker.Actions;
+using Satchel;
+
+namespace CycloneCopter {
+    public class CycloneCopter: Mod {
+        new public string GetName() => "CycloneCopter";
+        public override string GetVersion() => "1.0.0.0";
+
+        public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects) {
+            On.PlayMakerFSM.OnEnable += editFSM;
+        }
+
+        private void editFSM(On.PlayMakerFSM.orig_OnEnable orig, PlayMakerFSM self) {
+            orig(self);
+            if(self.gameObject.name == "Knight" && self.FsmName == "Nail Arts") {
+                FsmState extend = self.GetState("Cyclone Extend");
+                ((FloatAdd)extend.Actions[6]).add = 0.3f;
+                ((FloatAdd)extend.Actions[7]).add = 0.3f;
+                FloatAdd lift = new FloatAdd() {
+                    floatVariable = self.FsmVariables.GetFsmFloat("Y Velocity"),
+                    add = 20,
+                    everyFrame = false,
+                    perSecond = false
+                };
+                extend.InsertAction(lift, 2);
+            }
+        }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+# CycloneCopter
+
+A mod for the game Hollow Knight.
